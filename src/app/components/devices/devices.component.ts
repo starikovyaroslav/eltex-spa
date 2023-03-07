f1554e91307966a571480e6781632d000343b030
starikovyaroslav <slava.com.com90@gmail.com>
2023-03-08 00:34:23 +0700

    feat: Добавил логику добавления и получения списка устройств из localstorage

--- src/app/components/devices/devices.component.ts
+++ src/app/components/devices/devices.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IDevice } from '../../models/device';
 import { devices } from '../../data/devices';
 
@@ -7,6 +7,20 @@ import { devices } from '../../data/devices';
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss'],
 })
-export class DevicesComponent {
-  devices: IDevice[] = devices;
+export class DevicesComponent implements OnInit {
+  devices: IDevice[] = [];
+
+  // При инициализации список устройств проходит проверку на наличие данных в localstorage
+  ngOnInit(): void {
+    const deviceList = localStorage.getItem('deviceList');
+    if (this.devices.length == 0) {
+      //Если localstorage не пуст - берем из него список
+      if (deviceList !== null) {
+        this.devices = JSON.parse(deviceList);
+      } else {
+        this.devices = devices;
+        localStorage.setItem('deviceList', JSON.stringify(this.devices));
+      }
+    }
+  }
 }
