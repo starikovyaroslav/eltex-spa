3cda198ffc21a1c50bc85978da9931646bc30353
starikovyaroslav <slava.com.com90@gmail.com>
2023-03-07 23:12:41 +0700

    feat: создал небольшой мок-файл с параметрами устройств

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 import { DevicesComponent } from './components/devices/devices.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent,
-    DevicesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ListComponent, DevicesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/devices/devices.component.ts
+++ src/app/components/devices/devices.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { IDevice } from '../../models/device';
+import { devices } from '../../data/devices';
 
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss'],
 })
-export class DevicesComponent {}
+export class DevicesComponent {
+  devices: IDevice[] = devices;
+}
